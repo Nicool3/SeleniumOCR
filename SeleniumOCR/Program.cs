608987ee16b0e1e6cee8b30e25430e0bb30e1e12
Nicool3 <skq199101@sina.com>
2022-12-21 15:56:06 +0800

    添加项目文件。

---
+++ SeleniumOCR/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumOCR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
