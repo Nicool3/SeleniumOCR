ba3aa511a4c2d40f0799ae4308c6cccb249104e2
Nicool3 <skq199101@sina.com>
2022-12-21 21:33:56 +0800

    与已打开的浏览器进行连接。

--- SeleniumOCR/Program.cs
+++ SeleniumOCR/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumOCR
 {
@@ -10,6 +14,31 @@ namespace SeleniumOCR
     {
         static void Main(string[] args)
         {
+            ChromeDriverService driverService = ChromeDriverService.CreateDefaultService();
+            driverService.HideCommandPromptWindow = true; // 关闭cmd窗口
+
+            ChromeOptions options = new ChromeOptions();
+            options.BinaryLocation = @"D:\Program Files\Google\Chrome\Application\chrome.exe";
+            options.DebuggerAddress = "127.0.0.1:9527";
+            //options.AddArguments("--disk-cache-dir=./cache");
+            //options.AddArguments("--no-sandbox");
+            WebDriver driver = new ChromeDriver(driverService, options);
+            driver.Navigate().GoToUrl("https://pearocr.com/#/");
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+            IWebElement element = wait.Until((d) =>
+            {
+                try
+                {
+                    return driver.FindElement(By.Id("addImage"));
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            });
+
+            //element.Click();
         }
     }
 }
